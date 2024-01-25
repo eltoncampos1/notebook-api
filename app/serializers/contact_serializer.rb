536bc721eb2b87f21596e45c57a6e836283a11bf
eltoncampos1 <eltoncampos36@gmail.com>
2024-01-25 18:17:22 -0300

    chore: meta field

--- app/serializers/contact_serializer.rb
+++ app/serializers/contact_serializer.rb
@@ -5,6 +5,10 @@ class ContactSerializer < ActiveModel::Serializer
   has_many :phones
   has_one :address
 
+  meta do
+    {author: "eltoncampos1"}
+  end
+
   def attributes(*args)
     h = super(*args)
     h[:birthdate] = h[:birthdate].to_time.iso8601 unless object.birthdate.blank?
