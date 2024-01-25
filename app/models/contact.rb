0a762080e7220121a98cc8afde6d6ebdc5c11f5b
eltoncampos1 <eltoncampos36@gmail.com>
2024-01-25 09:50:58 -0300

    feat: i18n

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,19 +1,11 @@
 class Contact < ApplicationRecord
   belongs_to :kind, optional: true
 
-  def author 
-    "eltoncampos1"
-  end
-
-  def kind_description 
-    self.kind.description
-  end
-
-  def as_json(options = {})
-    super(
-      root: true,
-      methods: [:author, :kind_description],
-      include: {:kind => {only: :description}}
-    )
+  def to_br 
+    {
+      id: self.id,
+      name: self.name,
+      birthdate: (I18n.l(self.birthdate) unless self.birthdate.blank?),
+    }
   end
 end
